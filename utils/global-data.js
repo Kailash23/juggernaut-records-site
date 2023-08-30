1a1290ae312b155f86bd6cafa773639deab42a40
Kailash Uniyal <kuniyal@mafcarrefour.com>
2023-08-31 02:20:12 +0530

    Quick website changes

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,26 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+  const name = 'Juggernaut Records';
+
+  const line1 =
+    'From the soulful landscapes of Dehradun, emerges a hip-hop sensation that’s taking the scene by storm.';
+
+  const line2 =
+    "Saurabh Sam is not just any artist; he's a triple threat - a singer, an artist, and a lyricist whose pen game is as strong as his vocal delivery. Merging heartfelt lyrics with hard-hitting bars, Saurabh represents the true essence of hip-hop.";
+
+  const line3 =
+    'Dolla crafts the beats with his unique style. His impeccable production skills bring a unique flavor to the tracks, fusing modern beats with timeless rhythms, making every song an auditory experience.';
+
+  const line4 =
+    "Both artists come together under the powerhouse label, 'Juggernaut Records', synonymous with quality, authenticity, and the future of hip-hop.";
+
+  const footerText = '© 2023 Juggernaut Records. All Rights Reserved.';
 
   return {
     name,
-    blogTitle,
+    line1,
+    line2,
+    line3,
+    line4,
     footerText,
   };
 };
